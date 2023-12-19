a61fdacfc20bf68ede7bf5428bd69531ea3dccfe
Oscar Meneses <omenesess1800@alumno.ipn.mx>
2023-12-19 16:59:07 -0600

    Update layout max width and add link attributes

--- src/data/books.js
+++ src/data/books.js
@@ -1,37 +1,47 @@
 export const books = [
-    {
-      id: 'clean-code',
-      title: 'Clean Code: Manual de desarrollo ágil de software',
-      img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg",
-      opinion: 'Un clásico entre los clásicos del mundo de la programación. Aunque en pleno 2023 yo tengo una opinión un poco polémica. No me parece un libro que recomendaría a todo el mundo, especialmente a aquellas personas que están aprendiendo. Además, tiene un tono muy dogmático y existe gente que se lo toma, peligrosamente, como la biblia del desarrollo. Pero si eres capaz de leerlo tomando perspectiva, hay cosas interesantes que todavía están vigentes. Ojo que todos los ejemplos de código están basados en Java y mucha programación orientada a objetos.',
-      author: 'Robert C. Martin',
-    },
-    {
-      id: 'aprendiendo-git',
-      title: 'Aprendiendo Git',
-      img: "https://d2sofvawe08yqg.cloudfront.net/aprendiendo-git/s_hero2x?1628527844",
-      opinion: 'Un libro muy interesante para aprender Git. Mucho más allá de los comandos. Habla de buenas prácticas, estrategias de trabajo en equipo y flujos para mejorar la entrega de software. Además, está escrito por un autor español, lo que siempre es de agradecer.',
-      author: 'Miguel Angel Durán'
-    },
-    {
-      id: 'the-clean-coder',
-      title: 'The Clean Coder: A Code of Conduct for Professional Programmers',
-      img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg",
-      opinion: '"The Clean Coder: Un Código de Conducta para Programadores Profesionales" es un libro excepcional que revoluciona la forma en que los programadores abordan su trabajo. Con sus ideas prácticas e inspiradoras, establece un nuevo estándar de profesionalismo en la industria del desarrollo de software. Su enfoque en la responsabilidad personal, el aprendizaje continuo y la comunicación efectiva, promueve la entrega de código limpio, la mejora constante y la colaboración en equipo. En resumen, este libro es una herramienta indispensable que empodera a los programadores para alcanzar la excelencia y contribuir al éxito de sus organizaciones.',
-      author: 'Robert C. Martin',
-    },
-    {
-      id: 'javascript-the-good-parts',
-      title: 'JavaScript: The Good Parts',
-      img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg',
-      opinion: '"JavaScript: The Good Parts" es un libro imprescindible que destaca las mejores prácticas y características esenciales de JavaScript. A través de una visión concisa y perspicaz, el autor nos guía por el mundo de este lenguaje, revelando su potencial y resaltando sus aspectos más poderosos y efectivos. Con un enfoque en la simplicidad y la elegancia, el libro ofrece una guía práctica para aprovechar al máximo las partes más sólidas y confiables de JavaScript, permitiéndonos escribir un código claro y eficiente. En resumen, esta obra es una valiosa herramienta para cualquier desarrollador que desee dominar el arte de programar con JavaScript.',
-      author: 'Douglas Crockford',
-    },
-    {
-      id: 'eloquent-javascript',
-      title: 'Eloquent JavaScript: A Modern Introduction to Programming',
-      img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg',
-      opinion: '"Eloquent JavaScript" es un libro extraordinario que lleva a los lectores en un viaje apasionante a través del mundo de la programación en JavaScript. Con un enfoque amigable y accesible, el autor nos introduce en los conceptos fundamentales del lenguaje y nos guía hábilmente a través de ejercicios prácticos y desafiantes. Con una combinación equilibrada de teoría y práctica, el libro fomenta el pensamiento lógico y creativo, y nos enseña a desarrollar soluciones elegantes y eficientes. En resumen, "Eloquent JavaScript" es una guía imprescindible para aquellos que desean dominar este poderoso lenguaje de programación y explorar todo su potencial.',
-      author: 'Marijn Haverbeke',
-    }
-  ]
+  {
+    id: "clean-code",
+    title: "Clean Code: Manual de desarrollo ágil de software",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg",
+    opinion:
+      "Un clásico entre los clásicos del mundo de la programación. Aunque en pleno 2023 yo tengo una opinión un poco polémica. No me parece un libro que recomendaría a todo el mundo, especialmente a aquellas personas que están aprendiendo. Además, tiene un tono muy dogmático y existe gente que se lo toma, peligrosamente, como la biblia del desarrollo. Pero si eres capaz de leerlo tomando perspectiva, hay cosas interesantes que todavía están vigentes. Ojo que todos los ejemplos de código están basados en Java y mucha programación orientada a objetos.",
+    author: "Robert C. Martin",
+    link: "https://www.amazon.com.mx/Clean-Code-Handbook-Software-Craftsmanship/dp/0132350882/ref=sr_1_1?__mk_es_MX=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3F4ELBLLD90QL&keywords=clean+code&qid=1703026286&sprefix=clean+code%2Caps%2C119&sr=8-1&ufe=app_do%3Aamzn1.fos.4e545b5e-1d45-498b-8193-a253464ffa47",
+  },
+  {
+    id: "aprendiendo-git",
+    title: "Aprendiendo Git",
+    img: "https://d2sofvawe08yqg.cloudfront.net/aprendiendo-git/s_hero2x?1628527844",
+    opinion:
+      "Un libro muy interesante para aprender Git. Mucho más allá de los comandos. Habla de buenas prácticas, estrategias de trabajo en equipo y flujos para mejorar la entrega de software. Además, está escrito por un autor español, lo que siempre es de agradecer.",
+    author: "Miguel Angel Durán",
+    link: "https://leanpub.com/aprendiendo-git",
+  },
+  {
+    id: "the-clean-coder",
+    title: "The Clean Coder: A Code of Conduct for Professional Programmers",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg",
+    opinion:
+      '"The Clean Coder: Un Código de Conducta para Programadores Profesionales" es un libro excepcional que revoluciona la forma en que los programadores abordan su trabajo. Con sus ideas prácticas e inspiradoras, establece un nuevo estándar de profesionalismo en la industria del desarrollo de software. Su enfoque en la responsabilidad personal, el aprendizaje continuo y la comunicación efectiva, promueve la entrega de código limpio, la mejora constante y la colaboración en equipo. En resumen, este libro es una herramienta indispensable que empodera a los programadores para alcanzar la excelencia y contribuir al éxito de sus organizaciones.',
+    author: "Robert C. Martin",
+    link: "https://www.amazon.com/Clean-Coder-Conduct-Professional-Programmers/dp/0137081073?language=es_US",
+  },
+  {
+    id: "javascript-the-good-parts",
+    title: "JavaScript: The Good Parts",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg",
+    opinion:
+      '"JavaScript: The Good Parts" es un libro imprescindible que destaca las mejores prácticas y características esenciales de JavaScript. A través de una visión concisa y perspicaz, el autor nos guía por el mundo de este lenguaje, revelando su potencial y resaltando sus aspectos más poderosos y efectivos. Con un enfoque en la simplicidad y la elegancia, el libro ofrece una guía práctica para aprovechar al máximo las partes más sólidas y confiables de JavaScript, permitiéndonos escribir un código claro y eficiente. En resumen, esta obra es una valiosa herramienta para cualquier desarrollador que desee dominar el arte de programar con JavaScript.',
+    author: "Douglas Crockford",
+    link: "https://www.amazon.com/-/es/Douglas-Crockford/dp/0596517742/ref=sr_1_1?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2R43ABHSO5JUL&keywords=JavaScript%3A+The+Good+Parts&qid=1703026476&s=books&sprefix=javascript+the+good+parts%2Cstripbooks-intl-ship%2C120&sr=1-1",
+  },
+  {
+    id: "eloquent-javascript",
+    title: "Eloquent JavaScript: A Modern Introduction to Programming",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg",
+    opinion:
+      '"Eloquent JavaScript" es un libro extraordinario que lleva a los lectores en un viaje apasionante a través del mundo de la programación en JavaScript. Con un enfoque amigable y accesible, el autor nos introduce en los conceptos fundamentales del lenguaje y nos guía hábilmente a través de ejercicios prácticos y desafiantes. Con una combinación equilibrada de teoría y práctica, el libro fomenta el pensamiento lógico y creativo, y nos enseña a desarrollar soluciones elegantes y eficientes. En resumen, "Eloquent JavaScript" es una guía imprescindible para aquellos que desean dominar este poderoso lenguaje de programación y explorar todo su potencial.',
+    author: "Marijn Haverbeke",
+    link: "https://www.amazon.com/-/es/Marijn-Haverbeke/dp/1593279507/ref=sr_1_1?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=12SB5VLKC600Y&keywords=Eloquent+JavaScript%3A+A+Modern+Introduction+to+Programming&qid=1703026515&s=books&sprefix=eloquent+javascript+a+modern+introduction+to+programming%2Cstripbooks-intl-ship%2C118&sr=1-1",
+  },
+];
